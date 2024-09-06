0b664c76c72f50eecf1a9a1de8ff0127214deb8e
Merve Aydin <110039353+merveaydincode@users.noreply.github.com>
2024-09-06 23:06:23 +0300

    theme renkleri belirlendi.

--- src/styles/Colors.ts
+++ src/styles/Colors.ts
@@ -1,3 +1,17 @@
 export const Colors = {
+    primary: '#296573',
+    onPrimary: '#FFFFFF',
+    title: '#313131',
+    icon: '#555555',
+    border: '#DBDFE9',
+    cardborder: '#F1F1F4',
 
+    
+    text: {
+        primary: '#071437', // Ana metin rengi     
+        secondary: '#4B5675', // Alt metin rengi   
+        muted: '#252F4A', // Daha az belirgin metin rengi
+        inverted: '#FFFFFF', // Koyu arka planlar üzerindeki metin için 
+      },
+    
 }
